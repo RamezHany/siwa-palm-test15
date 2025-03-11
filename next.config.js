ba9ac9bb2afa3891a453263855d94f887eeb730d
RamezHany <rramezhany@gmail.com>
2025-03-11 02:37:27 +0200

    kkkkkkk

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['next-intl'],
+  },
+}
+
+module.exports = nextConfig 
